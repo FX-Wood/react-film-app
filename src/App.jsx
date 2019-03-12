a62f3249d00fabd7893bea682522822e7cc6764a
FX-Wood <fxwood2@gmail.com>
2019-03-12 16:47:26 -0700

    Importing TMDB, abstracted film listing and film details to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FilmListing from './FilmListing';
+import FilmDetails from './FilmDetails';
+import TMDB from './TMDB';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        div
+        <div className="filmLibrary">
+          <FilmListing films={TMDB.films}/>
+        </div>
+          <FilmDetails films={TMDB.films}/>
       </div>
     );
   }
