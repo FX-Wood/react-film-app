354d4a2b54cac111c1935118e87bfa483f894204
FX-Wood <fxwood2@gmail.com>
2019-03-14 13:38:30 -0700

    Fixed classname

--- src/FilmDetails.jsx
+++ src/FilmDetails.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class FilmDetails extends Component {
     render() {
         return (
-            <div className="film-list">
+            <div className="film-details">
                 <h1 className="section-title">FILMS</h1>
             </div>
         )
