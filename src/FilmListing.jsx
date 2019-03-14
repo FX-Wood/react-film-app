f1c11d594bff8ff6c50ff5df2e3a1918d4f68446
FX-Wood <fxwood2@gmail.com>
2019-03-14 01:24:05 -0700

    Finished click handling for all/fav filter

--- src/FilmListing.jsx
+++ src/FilmListing.jsx
@@ -4,13 +4,22 @@ import FilmRow from './FilmRow';
 class FilmListing extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            filter: 'all'
+        }
         this.handleFilterClick = this.handleFilterClick.bind(this)
     }
     handleFilterClick(e) {
-        console.log('handling filter click', e.target.innerText)
+        console.log('handling filter click', e.target.innerText.slice(0, -1).toLowerCase())
+        this.setState({
+            filter: e.target.innerText.slice(0,-1).toLowerCase()
+        })
     }
     render() {
-        var allFilms = this.props.films.map((film, i) => {
+        let all = ' '
+        let filter = ' '
+        this.state.filter === 'all' ? all += 'is-active' : filter += 'is-active'
+        let allFilms = this.props.films.map((film, i) => {
             return (
                 <FilmRow key={i} film={film} />
             )
@@ -19,11 +28,11 @@ class FilmListing extends Component {
         <div className="film-list">
             <h1 className="section-title">FILMS</h1>
             <div className="film-list-filters">
-                <div onClick={this.handleFilterClick} className="film-list-filter">
+                <div onClick={this.handleFilterClick} className={"film-list-filter" + all}>
                     ALL
                     <span className="section-count">{this.props.films.length}</span>
                 </div>
-                <div onClick={this.handleFilterClick} className="film-list-filter">
+                <div onClick={this.handleFilterClick} className={"film-list-filter" + filter}>
                     FAVES
                     <span className="section-count">0</span>
                 </div>
