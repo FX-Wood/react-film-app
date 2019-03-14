26ec9868414a8235201527d809fe226d44b46752
FX-Wood <fxwood2@gmail.com>
2019-03-14 01:07:08 -0700

    Added boilerplate and constructor

--- src/FilmListing.jsx
+++ src/FilmListing.jsx
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import FilmRow from './FilmRow';
 
 class FilmListing extends Component {
+    constructor(props) {
+        super(props)
+        this.handleFilterClick = this.handleFilterClick.bind(this)
+    }
+    handleFilterClick(e) {
+        console.log('handling filter click', e.target.innerText)
+    }
     render() {
         var allFilms = this.props.films.map((film, i) => {
             return (
@@ -9,8 +16,19 @@ class FilmListing extends Component {
             )
         })
         return (
-        <div className="film-details">
-            <h1 className="section-title">DETAILS</h1>
+        <div className="film-list">
+            <h1 className="section-title">FILMS</h1>
+            <div className="film-list-filters">
+                <div onClick={this.handleFilterClick} className="film-list-filter">
+                    ALL
+                    <span className="section-count">{this.props.films.length}</span>
+                </div>
+                <div onClick={this.handleFilterClick} className="film-list-filter">
+                    FAVES
+                    <span className="section-count">0</span>
+                </div>
+            </div>
+
             {allFilms}
         </div>
         )
