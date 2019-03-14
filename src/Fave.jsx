1445835fe4f4576798924a9e07b5b2026b646456
FX-Wood <fxwood2@gmail.com>
2019-03-14 00:24:05 -0700

    Adding fave component

---
+++ src/Fave.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Fave extends Component {
+    constructor(props) {
+        super(props)
+        this.handleClick = this.handleClick.bind(this); 
+    }
+    handleClick(e) {
+        console.log('handling click in Fave component');
+    }
+    render() {
+        return (
+            <div className="film-row-fave add_to_queue">
+                <p className="material-icons">add_to_queue</p>
+            </div>
+        )
+    }
+}
+
+export default Fave;
