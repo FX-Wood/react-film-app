a4bca90d3b9b7f9015ba2c011240fa3c358b1eb0
FX-Wood <fxwood2@gmail.com>
2019-03-14 01:06:52 -0700

    Added state and class toggle on click

--- src/Fave.jsx
+++ src/Fave.jsx
@@ -3,14 +3,25 @@ import React, { Component } from 'react';
 class Fave extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            isFave: false
+        }
         this.handleClick = this.handleClick.bind(this); 
     }
     handleClick(e) {
         console.log('handling click in Fave component');
+        this.setState( state => {
+            return {isFave: !state.isFave}
+        })
+        this.className ('add_to_queue')
+        this.classList.toggle('remove_from_queue')
     }
     render() {
+        let className = 'film-row-fave';
+        this.state.isFave? className += ' remove_from_queue': className += ' add_to_queue';
+
         return (
-            <div onClick={this.handleClick} className="film-row-fave add_to_queue">
+            <div onClick={this.handleClick} className={className}>
                 <p className="material-icons">add_to_queue</p>
             </div>
         )
