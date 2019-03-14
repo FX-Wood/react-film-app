7851c3ca202bd8eb68bc66d1c42e381fed58f9c7
FX-Wood <fxwood2@gmail.com>
2019-03-14 13:37:11 -0700

    Added constructor, lifted state

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,30 @@ import FilmDetails from './FilmDetails';
 import TMDB from './TMDB';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      films: TMDB.films,
+      current: {}
+    }
+            
+    this.handleDetailsClick = this.handleDetailsClick.bind(this)
+  }
+
+  handleDetailsClick(film) {
+    console.log('fetching details for ' + this.props.film.title)
+    this.setState({
+      current: film
+    })
+  }
+
   render() {
+    console.log('App rendering')
     return (
-      <div className="App">
         <div className="film-library">
-          <FilmListing films={TMDB.films}/>
+          <FilmListing onDetailsClick={this.handleDetailsClick} films={this.state.films}/>
+          <FilmDetails films={this.state.current}/>
         </div>
-          <FilmDetails films={TMDB.films}/>
-      </div>
     );
   }
 }
