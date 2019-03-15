b2b49e6dd796e3337ad5a15758b3e2d92b56c631
FX-Wood <fxwood2@gmail.com>
2019-03-15 10:57:49 -0700

    Refactored to functional component

--- src/Fave.jsx
+++ src/Fave.jsx
@@ -1,29 +1,17 @@
 import React, { Component } from 'react';
 
-class Fave extends Component {
-    constructor(props) {
-        super(props)
-        this.handleClick = this.handleClick.bind(this); 
-    }
-
-    handleClick(e) {
-        e.stopPropagation()
-        console.log('handling click in Fave component');
-        this.props.onFaveToggle()
-    }
-    
-    render() {
-        console.log('Fave rendering')
-        // check state to see if movie is in faves
-        let addRemove = this.props.isFave?'remove_from_queue' : 'add_to_queue';
-        
-
-        return (
-            <div onClick={this.handleClick} className={'film-row-fave ' + addRemove}>
-                <p className="material-icons">{addRemove}</p>
-            </div>
-        )
-    }
+function Fave(props) {
+    console.log('Fave rendering')
+    // check state to see if movie is in faves
+    let addRemove = props.isFave?'remove_from_queue' : 'add_to_queue';
+    return (
+        <div 
+            onClick={(e) => { e.stopPropagation(); props.onFaveToggle()}} 
+            className={'film-row-fave ' + addRemove}
+        >
+            <p className="material-icons">{addRemove}</p>
+        </div>
+    )
 }
 
 export default Fave;
