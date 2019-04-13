00180209ce59b5b222baa7149149088babfe0055
FX-Wood <fxwood2@gmail.com>
2019-04-12 17:15:24 -0700

    Destructuring props

--- src/FilmRow.jsx
+++ src/FilmRow.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import FilmPoster from './FilmPoster';
 import Fave from './Fave'
 
-function FilmRow(props) {
+export default function FilmRow({onDetailsClick, odc, isFave, film, onFaveToggle}) {
     return (
-        <div onClick={() => {props.onDetailsClick(props.film)}} className="film-row">
-            <FilmPoster path={props.film.poster_path} />
+        <div onClick={() => {onDetailsClick(film)}} className="film-row">
+            <FilmPoster path={film.poster_path} />
             <div className="film-summary">
-                <h1>{props.film.title}</h1>
-                <p>{props.film.release_date.slice(0,4)}</p>
+                <h1>{film.title}</h1>
+                <p>{film.release_date.slice(0,4)}</p>
             </div>
-            <Fave isFave={props.isFave} onFaveToggle={() => { props.onFaveToggle(props.film)}} />
+            <Fave isFave={isFave} onFaveToggle={() => { onFaveToggle(film)}} />
         </div>
     )
 }
-
-export default FilmRow;
